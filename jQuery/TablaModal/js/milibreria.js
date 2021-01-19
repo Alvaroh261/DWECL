9f2f8b1a139780623e478aa7ca5931ff73ecb337
AlvaroHM <alvaroh261@gmail.com>
2021-01-19 10:48:50 +0100

    DWECL

--- jQuery/TablaModal/js/milibreria.js
+++ jQuery/TablaModal/js/milibreria.js
@@ -3,53 +3,8 @@
 
 //window.addEventListener('load', function(){})
 $(document).ready(function () {
-
-
-  //eq(0) = thead[0]
-  $("thead").eq(0).css({ 'color': 'white', 'font-size': '1.em', 'background-color': 'gray' })
-
-  //Esto seria todas las filas pares del tbody
-  $("tbody tr:odd").css({ 'background-color': 'lightgray' })
-
-  //Esto seria un evento, que me dice al hacer click el texto que contiene el td
-  $("tbody td").click(function () {
-    //alert($(this).text());
-
-    if ($(this).css('color') === "rgb(0, 0, 0)") {
-      $(this).css({ 'color': 'red' });
-    } else {
-      $(this).css({ 'color': 'black' });
-    }
+  $("botonModal").click(function (){
+    console.log("a");
+    $("ventanaModal").css({"display": "block", "height": "30px","background-color": "yellow"});
   })
-
-
-  $("tbody td").click(function () {
-    //alert($(this).text());
-
-    if ($(this).text() != "") {
-      var input = $("<input>").attr("type", "text").val($(this).text());
-      $(this).empty().append(input);
-      input.focus();
-
-      input.blur(function() {
-        $(this).parent().html($(this).val());
-      })
-    }
-  })
-
-  //Al hacer el evento dobleclick quito esa fila
-  $("tbody tr").dblclick(function () {
-    $(this).hide();
-  })
-
-  //Al hacer el evento dobleclick muestro todos los tr del tbody ocultos
-  $("thead").dblclick(function () {
-    $("tbody tr").show();
-  })
-
-
-
-
-
-
 });
