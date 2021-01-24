78d254425b6b274d1c2f645d6eb5fec8bfe9abf5
Alvaroh261 <alvaroh261@gmail.com>
2021-01-24 13:09:56 +0100

    DWECL

--- jQuery/AjaxTiempo/js/milibreria.js
+++ jQuery/AjaxTiempo/js/milibreria.js
@@ -1,45 +1,37 @@
 //Aqui ya puedo utilizar la libreria de jquery
 
-
 //window.addEventListener('load', function(){})
 $(document).ready(function () {
 
-  $("#textCountry").keyup(function () {
-    var key = $(this).val();
-    var url = "https://restcountries.eu/rest/v2/name/"+ key;
+  $.ajax({
+    //Metodo que vamos a utilizar, (GET o POST)
+    type: "POST",
 
-    $.get(url, function (data, status) {
-      if(status == "success")
-      {
-        console.log(data);
-        select.empty();
-        var option;
-        for (let i = 0; i < data.length; i++) {
-          option = $("<option>").val(data[i].border.join(";")).text(data[i].name);
-          select.append(option);
-          peticionLandaza = false;
+    //URL de la api a utilizar
+    url: "http://api.ipstack.com/84.122.21.141?access_key=f74e1dac923a162a1305a516683f9a33",
 
-          option.click(function (e) { 
-            $.get("https://restcountries.eu/rest/v2/alpha?codes="+$(this).val(),
-            function (data, status) {
-              if(status == "success")
-              {
-                var caja = $("#bandera").empty();
-                for (let i = 0; i < data.length; i++) {
-                  $("<img>").attr({
-                    width: 35,
-                    height: 25,
-                    src: data[i].flag,
-                  });
-                  
-                }
-              }
-            });
-          });
-        }
-      }       
-    });
-  });
+    //Aqui se habilitaria si se va a enviar algo por GET
+    //data: "data",
 
-
+    //Que tipo de archivo nos devuelve la peticion AJAX
+    dataType: "json",
+  })
+    .done(function (data) {
+      leerTiempoCiudad(data.city);
+    })
 });
+
+//Hacemos una operacion callback
+function leerTiempoCiudad(ciudad) {
+  $.ajax({
+    type: "POST",
+    url: "http://api.openweathermap.org/data/2.5/weather?q=" + ciudad + "&appid=0bab6a49ff0be0b3dbbb3d47e1036fe0",
+    dataType: "json",
+  })
+    .done(function (data) {
+      var parrafo = $("<p>");
+      var resultado = (data.main.temp) -273.15;
+      parrafo.text("La temperatura en "+data.name+" es de "+Math.round(resultado)+"º C");
+      $("#pais").append(parrafo);
+    })
+}
