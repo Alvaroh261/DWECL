89eac7546d6a168a6da9cca33b9d1a2459489326
Alvaroh261 <alvaroh261@gmail.com>
2021-02-01 23:34:47 +0100

    DWECL

--- Proyectos/WhatsApp Web/libreria.js
+++ Proyectos/WhatsApp Web/libreria.js
@@ -10,41 +10,48 @@ $(document).ready(function () {
                     var contac = $("<p>").text(identificado.contactos[i].nombre);
                     $(divCon).append(contac);
                     $(divCon).addClass("border border-dark p-3 mb-2");
-                    $(divCon).click(function () { 
+                    $(divCon).click(function () {
                         $("#nombreChat").text(identificado.contactos[i].nombre);
 
+                        /* Conversaciones de la persona*/
+                        $.getJSON("./JSON/conversacion.json",
+                            function (data, textStatus, jqXHR) {
+                                if (textStatus == "success") {
+                                    var conversacion = data;
+                                    var tamConversacion = conversacion.length;
 
+                                    for (let i = 0; i < tamConversacion; i++) {
 
-/* COnversaciones de la persona
-                $.getJSON("./JSON/conversacion.json",
-                    function (data, textStatus, jqXHR) {
-                        if (textStatus == "success") {
-                            console.log(data);
-                        }
-                    }
-                ); */
+                                        var divConv = $("<div>");
+                                        var texto = $("<p>").text(conversacion[i].mensaje);
+                                        var fechaHora = $("<small>").text(conversacion[i].fechaHora);
+                                        $(divConv).append(texto);
+                                        $(divConv).append(fechaHora);
+                                        $(divConv).css({ "width": "50%" });
+                                        $(divConv).addClass("rounded my-2 mx-3");
 
+                                        if (identificado.mitelefono == conversacion[i].sender) {
+                                            $(divConv).css({ "background-color": "lightgreen", "float": "right"});
 
+                                        } else {
+                                            $(divConv).css({ "background-color": "white", "float": "left"});
 
+                                        }
 
-
-
-
-
-
-
-
-
-
-
-
+                                        $("#chat").append($(divConv));
+                                    }                                       
+                                    
+                                    $("#chat").scrollTop(9999999999999999999999999999e+10);
+                                }
+                            }
+                        );
 
                     });
-                    
+
                     $("#contactos").append($(divCon));
                 }
             }
-            
+
         }
     );
 });
